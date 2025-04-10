b098de6fbebbd784941fd6ccf7a87d1c8c9f0e10
Reine222 <koulaireine0222@gmail.com>
2025-04-10 17:01:44 +0000

    menu mobile

--- src/script.js
+++ src/script.js
@@ -7,3 +7,20 @@ if (window.scrollY > 10) {
     navbar.classList.remove("backdrop-blur-md", "bg-white/70", "shadow-md", "text-black");
 }
 });
+
+
+// Sidebar -->
+
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const sidebar = document.getElementById('sidebar');
+
+openBtn.addEventListener('click', () => {
+  // sidebar.style.width = '250px';
+  sidebar.classList.remove("hidden")
+  sidebar.classList.add("block"); // Ajoute la classe "active"
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.add("hidden"); // Ajoute la classe "active"
+});
